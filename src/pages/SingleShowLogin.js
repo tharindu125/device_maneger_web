df6ea3e74b59e5311b186b1a6bca4e512eeb0a01
tharindu125 <nawanjanaenda@gmail.com>
2023-04-05 13:39:50 +0530

    add search location function

--- src/pages/SingleShowLogin.js
+++ src/pages/SingleShowLogin.js
@@ -4,6 +4,31 @@ export default function SingleShowLogin() {
 
     const [l_name, setName] = useState('')
 
+    const SrarchLocation = async() => {
+      try {
+        const res = await fetch('/login',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({l_name})
+        })
+        const data = await res.json()
+        if (!res.ok) {
+            const {error} = data;
+            throw Error(error)
+        }
+        if (res.ok) {
+          alert("success")
+          window.location.href = `/SingleShow?l_name=${l_name}`
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+    }
+
+
 
   return (
     <div>
@@ -14,7 +39,7 @@ export default function SingleShowLogin() {
           <h1 className='SingleShowLogin_H'>What location Do you want to search?</h1>
             <input type='text' placeholder='Type here' value={l_name} onChange={e => setName(e.target.value)} />
             
-            <button className='location_btn' >Search</button>
+            <button className='location_btn' onClick={SrarchLocation} >Search</button>
            
           </div>
             
